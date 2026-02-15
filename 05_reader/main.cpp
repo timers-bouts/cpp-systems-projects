a44d2126a0e6cf4588c4d824eea42f1177ad8c09
John Brassey <pesos_jowly_0g@icloud.com>
2026-02-14 18:40:54 -0700

    Start project 5 and update old Makefiles

---
+++ 05_reader/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "telemetry/PacketWriter.h"
+#include "telemetry/TelemetryRecorder.h"
+#include "telemetry/TelemetryReader.h"
+
+int main() {
+
+    std::cout << "Telemetry Reader builds\n";
+    return 0;
+
+}
